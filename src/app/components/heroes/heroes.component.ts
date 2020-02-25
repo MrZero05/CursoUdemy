6a8bd0385910e5b9dff0740ca09d824498767b9d
Jose Daniel Osorio Morales <josorio@personalsoft.com.co>
2020-02-25 17:31:45 -0500

    Creación nuevo componente y nuevas rutas.

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -9,12 +10,17 @@ export class HeroesComponent implements OnInit {
 
   heroes: Heroe[] = [];
   // tslint:disable-next-line: variable-name
-  constructor(private _heroesService: HeroesService) {
+  constructor(private _heroesService: HeroesService, private _router: Router) {
     console.log('constructor');
    }
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
     console.log(this.heroes);
+  }
+
+  verHeroe(index: number) {
+    this._router.navigate(['/heroe', index]);
+
   }
 }
