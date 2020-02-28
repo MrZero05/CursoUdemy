eed5fd931edc80bad85097fd81a5b5db61d0e071
Jose Daniel Osorio Morales <josorio@personalsoft.com.co>
2020-02-28 16:46:15 -0500

    Cambios componentes para buscar heroe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroeComponent } from './components/heroe/heroe.component';
+import { SearchHeroeComponent } from './components/search-heroe/search-heroe.component';
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import { HeroeComponent } from './components/heroe/heroe.component';
     HomeComponent,
     AboutComponent,
     HeroesComponent,
-    HeroeComponent
+    HeroeComponent,
+    SearchHeroeComponent
   ],
   imports: [
     BrowserModule,
